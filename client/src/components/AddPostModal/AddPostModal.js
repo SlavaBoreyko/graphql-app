2b29fea7d3634d9842f8aed83311f0c2d75ed5a2
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-04-25 19:10:52 +0200

    Adding postCreate query to AddPostModal

--- client/src/components/AddPostModal/AddPostModal.js
+++ client/src/components/AddPostModal/AddPostModal.js
@@ -1,16 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import { gql, useMutation } from "@apollo/client"
+
+const CREATE_POST = gql`
+  mutation CreatePost($title: String!, $content: String!) {
+    postCreate(
+      post: {
+        title: $title,
+        content: $content
+      }
+    ) {
+      userErrors {
+        message
+      }
+      post {
+        title
+        content
+        published
+        createdAt
+        user {
+          name
+        }
+      }
+    }
+  }
+`
 
 export default function AddPostModal() {
+  const [createPost, { data, loading }] = useMutation(CREATE_POST)
+
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if(data){
+      if(data.postCreate.userErrors.length){
+        setError(data.postCreate.userErrors[0].message)
+      }
+    }
+  }, [data])
+
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    if(!content || !title) return
+    else {
+      createPost({
+        variables: {
+          title,
+          content
+        }
+      })
+      handleClose()
+    }
+  };
 
   return (
     <>
@@ -54,6 +103,7 @@ export default function AddPostModal() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
+          {error && <p>{error}</p>}
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
